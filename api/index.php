3cbbbe889293f2c7587ffe92859abb62b27c659b
Leeroy <51069447+leeroytm@users.noreply.github.com>
2023-06-02 13:21:33 +0800

    some tomfoolery

--- api/index.php
+++ api/index.php
@@ -4,4 +4,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 Tracy\Debugger::enable(Tracy\Debugger::DEVELOPMENT);
 
-throw new RuntimeException('Hello Tracy!');
+// Your PHP code to generate the content goes here
+$content = '<h1>Hello, PHP!</h1>';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Page</title>
+</head>
+<body>
+    <?php echo $content; ?>
+</body>
+</html>
